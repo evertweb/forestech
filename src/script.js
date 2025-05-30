4a6eeaef7d0a5b1a4641fd8df8ff0c4de5760159
evert cardenas <cardenasever072@gmail.com>
2025-05-30 17:55:17 -0500

    primera version

---
+++ src/script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Gracias por contactarte con Forestech de Colombia. Te responderemos pronto.');
+});
