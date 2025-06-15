f4bd8cfa2574169ba87e9f1a56de8d44e28b628b
FORESTECH-OFICINA <oficina.forestechprimavera@gmail.com>
2025-06-15 17:49:31 -0500

    app funciona

--- public/alimentacion/src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/Loader.jsx
-import React from 'react';
-
-function Loader() {
-    // Usamos el ID 'loader' para que el CSS que ya tienes lo estile automáticamente
-    return <div id="loader" style={{ display: 'flex' }}></div>;
-}
-
-export default Loader;
