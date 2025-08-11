ab6f635c2bd1e34f5edece80cf6d15523ad52435
hp-linux-evert <contacto@evertcardenas.com>
2025-08-11 14:17:40 -0500

    🔧 Remover restricciones commitlint - permitir commits libres
    Cambios:
    - Deshabilitadas todas las reglas de commitlint
    - Comentado hook commit-msg en husky
    - Commits ahora pueden tener formato libre sin restricciones
    
    Razón: Simplificar workflow de desarrollo

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: [],
   rules: {
-    'scope-enum': [2, 'always', [
-      'alimentacion', 'combustibles', 'shared', 'ci', 'docs', 'rules', 'repo'
-    ]]
+    // Disable all rules to allow free-form commit messages
+    'type-empty': [0],
+    'type-case': [0],
+    'subject-empty': [0],
+    'subject-case': [0],
+    'header-max-length': [0]
   }
 };
